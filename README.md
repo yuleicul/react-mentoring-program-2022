4c7ad015388220c67df60d7b8772c6c97e30107c
Yulei Chen <yulei_chen@epam.com>
2022-10-21 16:47:37 +0800

    Resolve `node_modules`

--- README.md
+++ README.md
@@ -15,3 +15,13 @@
   equal to `liveload`?
 
   How does it work?
+
+- Babel
+
+  babelHelpers?
+
+- import package from `node_module`
+
+  What does `@rollup/plugin-node-resolve`?
+
+  When to treat a package as an `external` package instead of resolving it?
--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,15 @@
 import livereload from 'rollup-plugin-livereload'
 import serve from 'rollup-plugin-serve'
 import { babel } from '@rollup/plugin-babel'
+import nodeResolve from '@rollup/plugin-node-resolve'
 
 export default {
   input: 'src/index.jsx',
   output: {
     file: 'dist/bundle.js',
     // format: 'cjs',
-    format: 'iife',
+    // format: 'iife',
+    format: 'es',
   },
   plugins: [
     process.env.NODE_ENV === 'development' &&
@@ -16,6 +18,7 @@ export default {
         port: 3000,
       }),
     process.env.NODE_ENV === 'development' && livereload(),
-    babel({ presets: ['@babel/preset-react'] }),
+    babel({ presets: ['@babel/preset-react'], babelHelpers: 'bundled' }),
+    nodeResolve(),
   ],
 }
