d05682897956bba07c4cda2856600a6b2b5d700e
Yulei Chen <yulei_chen@epam.com>
2022-12-10 17:51:33 +0800

    Done delete functionality

--- src/pages/HomePage/DeleteMovieModal/index.tsx
+++ src/pages/HomePage/DeleteMovieModal/index.tsx
@@ -1,17 +1,46 @@
 import styled from 'styled-components'
-import Form from '../../../common/Form'
+import InputButton from '../../../common/InputButton'
 import Modal from '../../../common/Modal'
+import { useDeleteMovie } from '../../../hooks/mutation'
+import { Movie } from '../../../store/moviesSlice'
 
+const FlexFooter = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  gap: 10px;
+  margin-top: 60;
+`
 interface DeleteMovieModalProps {
+  id: Pick<Movie, 'id'>
   onClose: () => void
+  onSuccess: () => void
 }
 
-const DeleteMovieModal: React.FC<DeleteMovieModalProps> = (props) => {
+const DeleteMovieModal: React.FC<DeleteMovieModalProps> = ({
+  id,
+  onClose,
+  onSuccess,
+}) => {
+  const [mutate] = useDeleteMovie({
+    onSuccess: () => {
+      onClose && onClose()
+      onSuccess && onSuccess()
+    },
+  })
+
   return (
-    <Modal title="DELETE MOVIE" width={686} onClose={props.onClose}>
-      <Form buttons={['confirm']}>
+    <Modal title="DELETE MOVIE" width={686} onClose={onClose}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          mutate(id)
+        }}
+      >
         <div className="text">Are you sure you want to delete this movie?</div>
-      </Form>
+        <FlexFooter>
+          <InputButton type="submit" value="CONFIRM" />
+        </FlexFooter>
+      </form>
     </Modal>
   )
 }
